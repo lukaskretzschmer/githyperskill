64bbca8ed00f8e8a3f2c086a0d3373ae5fdede8e
el kendort <wilfredo@wilfredoo.com>
2023-04-10 21:15:24 -0600

    Greet user by the name

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ def main() -> None:
     name = input("What's your name? ")
     print(f"Hello, {name}!")
     print("Nice to meet you!")
+
+
+if __name__ == "__main__":
+    print("Hello, world!")
+    main()
